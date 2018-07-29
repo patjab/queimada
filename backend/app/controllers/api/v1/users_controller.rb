6cc0f616204137f47b8d3d3389591ba451f6a2d7
prmoreira23 <prm.gredes@gmail.com>
2018-07-29 14:32:47 -0400

    User Controllers done.

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -16,13 +16,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(user_params)
-    # MESSAGE FOR PABLO FROM PATRICK:
-    #    Sorry, I had to edit this part to the way that Rishi was doing it during lecture because everytime I tried to pass in strong params
-    #    for User.new(...).save, it was always rejecting the password and password_confirmation fields that should exist in the params.require(:user) hash.
-    #    Password and password_confirmation were present in the outer params hash, but not in the nested params[:user] hash, leading User.new(user_params)
-    #    to be processed without the password and subsequently a 422 unprocessable_entity error due to the lack of password.
-    byebug
     @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created
