dbf9b82b0cb7de405546d751eda59c46153b256e
github username <github email address>
2018-08-01 13:33:46 -0400

    styled video chat

--- frontend/src/components/FriendInfo.js
+++ frontend/src/components/FriendInfo.js
@@ -8,13 +8,21 @@ export default class FriendInfo extends Component {
       <div className="ui row" style={{"paddingBottom":"30px"}}>
         {
           Object.keys(this.props.currentFriend).length === 0 ?
-            <h1>
+            <h1 style={{"textAlign":"center"}}>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
               Please choose a user to get started
+              <br/>
+              <br/>
+              <br/>
+              <br/>
             </h1>
           :
             <div className="ui link cards float left">
               <FriendInfoCard friend={this.props.currentFriend} deleteFriend={this.props.deleteFriend} />
-              <div>
+              <div style={{"margin":"auto"}}>
                 <VideoChat currentFriend={this.props.currentFriend} currentUser={this.props.currentUser}/>
               </div>
             </div>
--- frontend/src/components/VideoChat.js
+++ frontend/src/components/VideoChat.js
@@ -10,9 +10,6 @@ export default class VideoChat extends Component {
       let username = this.props.currentUser.email
       let roomName = [this.props.currentUser.email, this.props.currentFriend.email].sort().join("")
 
-      console.log("YOUR USERNAME WILL BE ", username)
-      console.log("THE ROOM NAME WILL BE ", roomName)
-
       const localVideoEl = $('#local-video');
       const remoteVideoTemplate = Handlebars.compile($('#remote-video-template').html());
       const remoteVideosEl = $('#remote-videos');
@@ -36,28 +33,71 @@ export default class VideoChat extends Component {
       webrtc.on('videoAdded', (video, peer) => {
         const id = webrtc.getDomId(peer);
         const html = remoteVideoTemplate({ id });
+
+        let localVideoEl = document.getElementById('local-video')
+        let remoteVideoEl = document.getElementById('remote-videos').firstChild
+
+        console.log(localVideoEl)
+        console.log(remoteVideoEl)
+
         $('#allVideos').append(html);
         $(`#${id}`).html(video);
-        $(`#${id}`).addClass('ui image small'); // Make video element responsive
+        $(`#${id}`).addClass('ui image large'); // Make video element responsive
+
+        // {{"backgroundColor":"black", "color":"white", "fontSize":"30px", "textAlign":"center", "padding":"12px"}}
+        // "backgroundColor: black; color: white; fontSize: 30px; textAlign: center; padding: 12px;"
+        document.getElementById(`topStatus`).style['background-color'] = 'black'
+        document.getElementById(`topStatus`).style['color'] = 'white'
+        document.getElementById(`topStatus`).style['font-size'] = '40px'
+        document.getElementById(`topStatus`).style['text-align'] = 'center'
+        document.getElementById(`topStatus`).style['padding'] = '12px'
+        document.getElementById(`topStatus`).appendChild(document.createElement('BR'))
+        document.getElementById(`topStatus`).innerHTML = `Secure Chat`
+
+
+        document.getElementById(`statusBar`).style['background-color'] = 'black'
+        document.getElementById(`statusBar`).style['color'] = 'white'
+        document.getElementById(`statusBar`).style['font-size'] = '30px'
+        document.getElementById(`statusBar`).style['text-align'] = 'center'
+        document.getElementById(`statusBar`).style['padding'] = '12px'
+        document.getElementById(`statusBar`).appendChild(document.createElement('BR'))
+        document.getElementById(`statusBar`).appendChild(document.createTextNode(`You are now privately chatting with ${this.props.currentFriend.full_name}`))
+        document.getElementById(`statusBar`).appendChild(document.createElement('BR'))
+        document.getElementById(`statusBar`).appendChild(document.createElement('BR'))
+
+        const hangUpButton = document.createElement('BUTTON')
+
+        hangUpButton.setAttribute('id', 'hangUpButton')
+        hangUpButton.innerHTML = 'Hang Up'
+        hangUpButton.classList = 'ui button negative'
+
+        document.getElementById(`statusBar`).appendChild(hangUpButton)
+
+        document.getElementById(`allVideos`).style['background-color'] = 'black'
+
+        document.getElementById(`hangUpButton`).addEventListener('click', ()=> {
+          window.location.reload()
+        })
       });
     }
   }
 
   render() {
-    return (<Fragment>
-      <span id="allVideos" className="video-container">
-        <video id="local-video" className="ui big image hidden" autoplay></video>
-        <div className="overlay-desc">
-          { (this.props.currentUser && Object.keys(this.props.currentFriend).length !== 0) ?
-              <button id="join-btn" className="ui submit green massive transparent button" style={{"opacity":"0.6"}}>Call {this.props.currentFriend.full_name}</button>
-                : null
-          }
-        </div>
-        <div className="overlay-desc-2">
-          <span id="remote-videos" className="ui big small"></span>
-        </div>
-      </span>
+    return (<div style={{"paddingTop":"20px"}}>
+      <div id="allVideos" className="video-container">
+          <div id="topStatus"></div>
+          <span id="remote-videos" className="ui large" style={{"margin":"20px"}}></span>
+          <video id="local-video" className="ui large image hidden" style={{"margin":"20px"}} autoplay></video>
+          <div className="overlay-desc">
+            { (this.props.currentUser && Object.keys(this.props.currentFriend).length !== 0) ?
+                <button id="join-btn" className="ui submit green massive transparent button" style={{"opacity":"0.6"}}>Call {this.props.currentFriend.full_name}</button>
+                  : null
+            }
+          </div>
+      </div>
+      <div id="statusBar"></div>
+
       <script id="remote-video-template" type="text/x-handlebars-template"></script>
-    </Fragment>)
+    </div>)
   }
 }
